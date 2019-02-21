254d97754e43495bc2d36be18f3093371ab743e9
ishankoul <ishan@blockgemini.com>
2019-02-21 16:26:58 +0400

    todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Todos from './components/Todos'
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Gym',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting',
+        completed: false
+      }
+    ]
+  }
+// Toggle complete
+  markComplete = (id) => {
+    console.log('From ', id);
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })
+  })
+}
+
+delTodo = (id) => {
+  this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]});
+}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Todos todos={this.state.todos} markComplete={this.markComplete}
+        delTodo= {this.delTodo}/>
+        <h1>App</h1>
       </div>
     );
   }
